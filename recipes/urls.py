751ef05197d8d0ae660947ca9997d51ea3435df0
Rodrigo Ricci <rodrigo@ricci.dev.br>
2022-11-14 19:56:39 -0300

    Ajustes gerais

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from recipes.views import home
+
+from recipes import views
 
 urlpatterns = [
-    path('', home)
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipe, name="recipes"),
 ]
