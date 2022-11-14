592598535e160ab4367390235afcdcd03f7f16f5
Rodrigo Ricci <rodrigo@ricci.dev.br>
2022-11-14 20:09:56 -0300

    inclusao app_name para faciliar a chamada da url

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from recipes import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home, name="recipes-home"),
+    path('', views.home, name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipes"),
 ]
