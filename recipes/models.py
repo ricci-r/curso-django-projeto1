0052e922b00f8d579263821385c2eb1f088ab96a
Rodrigo Ricci <rodrigo@ricci.dev.br>
2022-11-14 20:47:09 -0300

    Inclusão models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=50)
+    servings = models.CharField(max_length=50)
+    servings_unit = models.CharField(max_length=50)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y-%m-%d/')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
